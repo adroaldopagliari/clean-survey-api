f9dda44ad3a405e27cf3a5bb331266b72c05bdfb
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-01-02 18:28:08 -0300

    test: ensure signUpController returns 400 is the email was not provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,12 +4,28 @@ describe('SignUp Controller', () => {
   it('Should return 400 if name was not provided', () => {
     const sut = new SignUpController();
     const httpRequest = {
-      name: 'example',
-      email: 'example@example.com',
-      passwordConfirmation: '123456',
+      body: {
+        email: 'example@example.com',
+        password: '123456',
+        passwordConfirmation: '123456',
+      },
     };
     const httpResponse = sut.handle(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error('Missing param: name'));
+  });
+
+  it('Should return 400 if e-mail was not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: 'example',
+        password: '123456',
+        passwordConfirmation: '123456',
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'));
   });
 });
