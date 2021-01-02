16e70ecfd5adc1390212bdc0512964817e0f7be6
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-01-02 17:24:00 -0300

    chore: adjust jest config files

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,14 @@
+import SignUpController from './signup';
+
+describe('SignUp Controller', () => {
+  it('Should return 400 if name was not provided', () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      name: 'example',
+      email: 'example@example.com',
+      passwordConfirmation: '123456',
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
