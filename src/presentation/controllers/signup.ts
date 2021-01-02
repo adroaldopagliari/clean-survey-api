b2f922a7a56fafdb9f70196796f593d3c6cf90d5
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-01-02 18:27:31 -0300

    feat: ensure signUpController returns 400 is the email was not provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,21 @@
 export default class SignUpController {
-  handle(httpRequest: unknown): any {
-    return {
-      request: httpRequest,
-      statusCode: 400,
-      body: new Error('Missing param: name'),
-    };
+  handle(httpRequest: any): any {
+    if (!httpRequest.body.name) {
+      return {
+        request: httpRequest,
+        statusCode: 400,
+        body: new Error('Missing param: name'),
+      };
+    }
+
+    if (!httpRequest.body.email) {
+      return {
+        request: httpRequest,
+        statusCode: 400,
+        body: new Error('Missing param: email'),
+      };
+    }
+
+    return null;
   }
 }
