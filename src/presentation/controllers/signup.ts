e6cc789379515af3c51e6d7f7910898fa0b026ab
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-01-02 22:03:49 -0300

    refactor: simplify if statments

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,12 +4,11 @@ import { HttpRequest, httpResponse } from '../protocols/http';
 
 export default class SignUpController {
   handle(httpRequest: HttpRequest): httpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'));
-    }
+    const requiredFields = ['name', 'email'];
 
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'));
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field])
+        return badRequest(new MissingParamError(field));
     }
 
     return null;
