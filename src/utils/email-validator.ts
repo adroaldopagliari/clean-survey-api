292f99c3adde7d7ee3159795ed5c43c294fc959e
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-01-11 23:19:34 -0300

    test: ensure EmailValidatorAdapter returns true if validator returns true

--- src/utils/email-validator.ts
+++ src/utils/email-validator.ts
@@ -1,8 +1,9 @@
+import validator from 'validator';
 import { EmailValidator } from '../presentation/protocols/email-validator';
 
 // eslint-disable-next-line import/prefer-default-export
 export class EmailValidatorAdapter implements EmailValidator {
-  isValid(_email: string): boolean {
-    return false;
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
   }
 }
