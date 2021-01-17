8e8d9e491d3e39fc8a6016b9807f2ab8682998f2
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-01-16 21:47:42 -0300

    test: ensure dbAddAccout throws an error if addAccountRepository throws

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -95,4 +95,24 @@ describe('DbAddAccount UseCase', () => {
       password: 'hashed_password',
     });
   });
+
+  it('Should throw if Encrypter throws an error', async () => {
+    const { addAccountRepositoryStub, sut } = makeSut();
+
+    jest
+      .spyOn(addAccountRepositoryStub, 'add')
+      .mockReturnValueOnce(
+        new Promise((resolve, reject) => reject(new Error())),
+      );
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+
+    const promise = sut.add(accountData);
+
+    await expect(promise).rejects.toThrow();
+  });
 });
