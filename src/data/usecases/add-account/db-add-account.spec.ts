fef85e264735b1d3b9c9d423070f58ed171d65e9
Adroaldo J. Pagliari <adroaldo@outlook.com>
2021-01-16 21:51:06 -0300

    test: ensure dbAddAccount returns an account on sucess

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -115,4 +115,22 @@ describe('DbAddAccount UseCase', () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  it('Should return an account on sucess', async () => {
+    const { sut } = makeSut();
+
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password',
+    };
+    const account = await sut.add(accountData);
+
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password',
+    });
+  });
 });
